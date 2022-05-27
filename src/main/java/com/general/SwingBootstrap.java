9ee576f12923d3921f6cea03bb567ebd3c8a3fd4
zhouchenhua <noatnu@foxmail.com>
2022-05-27 15:59:44 +0800

    idea theme

--- src/main/java/com/general/SwingBootstrap.java
+++ src/main/java/com/general/SwingBootstrap.java
@@ -5,20 +5,46 @@ import com.common.utils.RunAbs;
 import com.formdev.flatlaf.FlatLightLaf;
 import com.general.menu.JMenuBarTool;
 import com.general.panel.PanelTool;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class SwingBootstrap {
-
+    //初始化的时候 路径设置为项目路径 E:/IdeaProjects/swing-utils
+    public static String BASE_DIR = "";
+    //初始化的时候 路径设置为类路径 E:/IdeaProjects/swing-utils/target/classes
+    public static String BASE_CLASS_DIR = "";
     private final static Logger logger = LoggerFactory.getLogger(SwingBootstrap.class);
+    /**
+     * 设置路径
+     */
+    private static void settingPath() {
+        final String separatorChars = File.separator;
+        String path = Thread.currentThread().getContextClassLoader().getResource("images/cloud_74.147368421053px_1258173_easyicon.net.png").getPath();
+        //path前面有可能多出一个/所以用file 中转一下
+        File file = new File(path);
+        System.out.println(path);
+        path = StringUtils.remove(file.getPath(), "file:\\");
+        BASE_DIR = StringUtils.substringBeforeLast(path, "swing-utils");
+        System.out.println(BASE_DIR);
+        BASE_CLASS_DIR = BASE_DIR + separatorChars + "classes";
+        File dirFile = new File(BASE_CLASS_DIR);
+        //假如没有目录存在 则创建
+        if (!dirFile.exists()) {
+            dirFile.mkdirs();
+        }
+
+    }
 
     public static void execute() {
+        settingPath() ;
         SwingUtilities.invokeLater(() -> {
             try {
                 run();
